bdec1b112859dc9c74313317ffa0019ce8ed59ef
Andrew Markwell <andrew.markwell@nhs.net>
2016-11-03 17:00:18 +0000

    changed the template to lookup from dsr/Sprint_4/login/dashboard -> dsr/Sprint_4/login/dashboard/index

--- app/routes.js
+++ app/routes.js
@@ -440,7 +440,7 @@ module.exports = {
 
 	app.get('/dsr/Sprint_4/login/dashboard', function(req, res) {
 		
-		res.render('dsr/Sprint_4/login/dashboard',
+		res.render('dsr/Sprint_4/login/dashboard/index',
         {
 			"totalUnpaidItemsCount" : pendingItemsJson.length + rejectedItemsJson.length,
 			"rejectedItemsCount" : rejectedItemsJson.length,
